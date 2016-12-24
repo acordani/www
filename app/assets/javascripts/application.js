3d3f32c21703d5600354658842276764f9f05cda
ac <alcordani@gmail.com>
2016-12-24 18:59:06 +0100

    home page suite

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,8 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+$('.image_link').hover(
+  function(){ $(this).stop().animate({ opacity : '0.5' }); },
+  function(){ $(this).stop().animate({ opacity : '1' }); }
+ );
