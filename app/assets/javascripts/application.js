abd4bdb91bd87b424ac306d351c25329ac6a8cf6
ac <alcordani@gmail.com>
2016-12-28 19:42:06 +0100

    Debut page REALTOR

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,13 @@ $('.image_link').hover(
   function(){ $(this).stop().animate({ opacity : '0.5' }); },
   function(){ $(this).stop().animate({ opacity : '1' }); }
  );
+
+$(document).ready(function() {
+  $('.hire_me').on('click', function() {
+    $('.modal').addClass('is-active')
+  })
+
+  $('.modal').on('click', function() {
+    $(this).removeClass('is-active')
+  })
+})
