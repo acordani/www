9300ef0f6ccacba6845a13dae0dd95b710840fbd
ac <alcordani@gmail.com>
2017-01-22 11:25:58 +0100

    Prices Controller & Show page

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -20,14 +20,18 @@ class PricesController < ApplicationController
 
 	def show
 		@price = Price.find(params[:id])
+		@average_price_house = Price.average(:price_average_house)
+		@average_price_apartment = Price.average(:price_average_apartment)
 	end
+
+	
 
 
 
 	private
 
 	def price_params
-		params.require(:price).permit(:price_average_house, :price_max_house, :price_min_apartment, :price_average_apartment, :price_max_apartment, :street, :city, :neighborhood, :type_of_street)
+		params.require(:price).permit(:price_min_house, :price_average_house, :price_max_house, :price_min_apartment, :price_average_apartment, :price_max_apartment, :street, :city, :neighborhood, :type_of_street)
 	end
 
 
