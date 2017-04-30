65f3558039e42da505be05a9fd743c9f0af0299e
ac <alcordani@gmail.com>
2017-04-30 19:45:02 +0200

    f

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,14 @@ require_relative 'boot'
 
 require 'rails/all'
 
+require "sprockets/railtie"
+require "attachinary/orm/active_record"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+
 
 module WWW
   class Application < Rails::Application
