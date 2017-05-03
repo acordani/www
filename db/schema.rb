7b20d3bdca602c79935b137bc8b7240a414159fd
ac <alcordani@gmail.com>
2017-05-03 19:56:02 +0200

    add prospect model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170131121137) do
+ActiveRecord::Schema.define(version: 20170503105903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,16 @@ ActiveRecord::Schema.define(version: 20170131121137) do
     t.string   "slug"
     t.string   "address"
     t.string   "zip_code"
+  end
+
+  create_table "prospects", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "society"
+    t.string   "phone_number"
+    t.string   "mail"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "sales", force: :cascade do |t|
