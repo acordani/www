e55d2e4fb6926aa0dc67a87df27fbb943702a9bf
ac <alcordani@gmail.com>
2017-08-13 11:26:42 +0200

    facebook partage show

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -10,5 +10,13 @@ module MetaTagsHelper
   def meta_image
     content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META['meta_image']
   end
+
+  def facebook_meta_title
+    content_for?(:facebook_meta_title) ? content_for(:facebook_meta_title) : DEFAULT_META["facebook_meta_title"]
+  end
+
+  def facebook_meta_description
+    content_for?(:facebook_meta_description) ? content_for(:facebook_meta_description) : DEFAULT_META["facebook_meta_description"]
+  end
 end
 
