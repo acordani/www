61992131454b021e988010beb55dc8203c460d47
ac <alcordani@gmail.com>
2017-09-09 14:46:25 +0200

    ajout models ds rails admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -4,7 +4,7 @@ RailsAdmin.config do |config|
   config.authorize_with do |controller|
     redirect_to main_app.root_path unless current_user && current_user.admin
   end
-  config.included_models = ["Sale", "Neighborhood", "Sample"]
+  config.included_models = ["Sale", "Neighborhood", "Sample", "Land", "City", "Citysearch"]
 
   ### Popular gems integration
 
