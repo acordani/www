5c5e3fee85e4c5e8e7936af364540e7bba27e77b
alexandrecordani <alcordani@gmail.com>
2018-10-11 11:00:44 +0200

    retire footer ed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,5 @@
 //= require jquery
-
 //= require jquery_ujs
-
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
@@ -9,21 +7,11 @@
 //= require bootstrap-table
 //= require underscore
 //= require gmaps/google
-
-
-
-
-
-
-
 //= require ./google_maps_autocomplete
 //= require ./init_attachinary
 //= require ./typed_home
 //= require jquery.raty
-
-
 //= require typed
-
 //= require react
 //= require react_ujs
 //= require components
